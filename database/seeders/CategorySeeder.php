e628b24d26cac41f2086250673fcc0c8d73a5ad3
meryem SH <meryem2003020@gmail.com>
2023-05-22 09:25:42 +0100

    AddStylePageCtegory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,13 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::create(["name" => "Vêtements et Mode", "image" => '/fake/products/vetementCat.webp']);
-        Category::create(["name" => "Électronique et Outillage", "image" => '/fake/products/EleCate.gif']);
-        Category::create(["name" => "Maison et Jardin", "image" => '/fake/products/maisonCat.gif']);
-        Category::create(["name" => "Santé et Beauté", "image" =>'/fake/products/beauteCat.gif']);
-        Category::create(["name" => "Sports et Remise en Forme", "image" =>'/fake/products/sportCat.gif']);
-        Category::create(["name" => "jouets et Games", "image" => '/fake/products/jouCat.gif']);
-        Category::create(["name" => "Livres et Médias", "image" => '/fake/products/livreCat.gif']);
+        Category::create(["name" => "Vêtements et Mode", "image" => '/fake/category/vetement.jpg']);
+        Category::create(["name" => "Électronique et Outillage", "image" => '/fake/category/electro.jpg']);
+        Category::create(["name" => "Maison et Jardin", "image" => '/fake/category/home.jpg']);
+        Category::create(["name" => "Santé et Beauté", "image" =>'/fake/category/beaute.jpg']);
+        Category::create(["name" => "Sports et Remise en Forme", "image" =>'/fake/category/sport.jpg']);
+        Category::create(["name" => "jouets et Games", "image" => '/fake/category/games.jpg']);
+        Category::create(["name" => "Livres et Médias", "image" => '/fake/category/books.jpg']);
 
     }
 }
